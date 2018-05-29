a6acf939cadd12f25b7558042426ad516943b0bc
Vadim <chuvak117@gmail.com>
2018-05-29 19:55:44 +0300

    Removed JQuery

--- template/static/index.js
+++ template/static/index.js
@@ -1,5 +1,4 @@
-$(function () {
-
+document.addEventListener("DOMContentLoaded", function (event) {
     updateProxy();
     proxyButton();
 
@@ -10,22 +9,25 @@ $(function () {
     setInterval(function () {
         updateProxy();
     }, 30000);
-
 });
 
 function updateProxy() {
-    $.getJSON("/json", function (data) {
-        $("#num").html(data.Proxies.length + " proxies");
-        $("textarea").html(data.Proxies.join("\n"));
-    }
-    );
+    fetch('/json')
+        .then(res => res.json())
+        .then((out) => {
+            document.getElementById('num').innerHTML = out.Proxies.length + ' proxies';
+            document.getElementsByTagName('textarea')[0].innerHTML = out.Proxies.join("\n");
+        })
+        .catch(err => { throw err; });
 }
 
 function proxyButton() {
-    $.getJSON("/json", function (data) {
-        randomNumber = Math.floor((Math.random() * data.Proxies.length) + 0);
-        randomProxy = data.Proxies[randomNumber].split(":");
-        $("#b").attr("href", 'tg://socks?server=' + randomProxy[0] + '&port=' + randomProxy[1]);
-    }
-    );
+    fetch('/json')
+        .then(res => res.json())
+        .then((out) => {
+            randomNumber = Math.floor((Math.random() * out.Proxies.length) + 0);
+            randomProxy = out.Proxies[randomNumber].split(":");
+            document.getElementById('b').setAttribute("href", 'tg://socks?server=' + randomProxy[0] + '&port=' + randomProxy[1]);
+        })
+        .catch(err => { throw err; });
 }
